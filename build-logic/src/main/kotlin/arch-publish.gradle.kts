e870e92cbc304ba6b7ede9c41ae0dc0c8eaa45ea
Matheus Barbieri Corregiari <metheus.92@gmail.com>
2026-06-25 11:11:03 -0300

    chore: refresh build tooling

--- build-logic/src/main/kotlin/arch-publish.gradle.kts
+++ build-logic/src/main/kotlin/arch-publish.gradle.kts
@@ -8,6 +8,7 @@ import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinMultiplatform
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import com.vanniktech.maven.publish.SourcesJar
+import org.gradle.plugins.signing.Sign
 
 plugins {
     `maven-publish`
@@ -56,4 +57,15 @@ extensions.configure(MavenPublishBaseExtension::class) {
             sourcesJar = SourcesJar.Sources(),
         ),
     )
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf {
+        val localPublish = gradle.taskGraph.allTasks.any {
+            it.name == "ciPublishLocal" ||
+                it.name == "publishToMavenLocal" ||
+                it.name.endsWith("ToMavenLocal")
+        }
+        !localPublish
+    }
 }
